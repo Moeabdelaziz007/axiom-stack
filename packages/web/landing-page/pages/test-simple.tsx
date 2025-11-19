89795fa932e8c3dccf414171bc76c96decbe38f7
Mohamed Abdelaziz <amrikyy@gmail.com>
2025-11-19 07:55:37 +0200

    Add simple test page to verify Next.js is working

---
+++ packages/web/landing-page/pages/test-simple.tsx
@@ -0,0 +1,9 @@
+// pages/test-simple.tsx - Simple test page
+export default function TestSimplePage() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Simple Test Page</h1>
+      <p>This is a simple test page to verify Next.js is working.</p>
+    </div>
+  );
+}
