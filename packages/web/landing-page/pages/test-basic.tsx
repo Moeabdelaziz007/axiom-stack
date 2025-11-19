58828c10780f4e373e95e99435b37417cd38959b
Mohamed Abdelaziz <amrikyy@gmail.com>
2025-11-19 07:59:50 +0200

    Add basic test page to verify Next.js is working

---
+++ packages/web/landing-page/pages/test-basic.tsx
@@ -0,0 +1,9 @@
+// pages/test-basic.tsx - Basic test page
+export default function TestBasicPage() {
+  return (
+    <div>
+      <h1>Basic Test Page</h1>
+      <p>This is a basic test page.</p>
+    </div>
+  );
+}
