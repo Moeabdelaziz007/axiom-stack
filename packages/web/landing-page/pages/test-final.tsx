b9fe056348b4d31a914e5338c5e114bff3dacc59
Mohamed Abdelaziz <amrikyy@gmail.com>
2025-11-19 08:03:24 +0200

    Add final test page to verify Next.js is working

---
+++ packages/web/landing-page/pages/test-final.tsx
@@ -0,0 +1,9 @@
+// pages/test-final.tsx - Final test page
+export default function TestFinalPage() {
+  return (
+    <div>
+      <h1>Final Test Page</h1>
+      <p>This is the final test page.</p>
+    </div>
+  );
+}
